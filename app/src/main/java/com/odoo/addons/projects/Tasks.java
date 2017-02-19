6c5b6f4334718ebc1c293b35685597f98382b097
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-02-18 20:20:56 -0500

    ProjectTask Crud

--- app/src/main/java/com/odoo/addons/projects/Tasks.java
+++ app/src/main/java/com/odoo/addons/projects/Tasks.java
@@ -3,6 +3,7 @@ package com.odoo.addons.projects;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -10,16 +11,25 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.odoo.R;
 import com.odoo.addons.projects.models.ProjectTask;
 import com.odoo.core.orm.ODataRow;
 import com.odoo.core.support.addons.fragment.BaseFragment;
+import com.odoo.core.support.addons.fragment.IOnSearchViewChangeListener;
 import com.odoo.core.support.addons.fragment.ISyncStatusObserverListener;
 import com.odoo.core.support.drawer.ODrawerItem;
 import com.odoo.core.support.list.OCursorListAdapter;
+import com.odoo.core.utils.IntentUtils;
 import com.odoo.core.utils.OControls;
+import com.odoo.core.utils.OCursorUtils;
+import com.odoo.core.utils.OResource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,30 +38,40 @@ import java.util.List;
  * Created by Ricardo Livelli on 09/02/2017.
  */
 
-public class Tasks extends BaseFragment implements LoaderManager.LoaderCallbacks<Cursor>,
-        ISyncStatusObserverListener, SwipeRefreshLayout.OnRefreshListener,OCursorListAdapter.OnViewBindListener {
+public class Tasks extends BaseFragment implements ISyncStatusObserverListener,
+        LoaderManager.LoaderCallbacks<Cursor>, SwipeRefreshLayout.OnRefreshListener,
+        OCursorListAdapter.OnViewBindListener, IOnSearchViewChangeListener, View.OnClickListener,
+        AdapterView.OnItemClickListener {
 
     public static final String KEY = Tasks.class.getSimpleName();
-
     private View mView;
+    private String mCurFilter = null;
     private ListView listView;
-    private OCursorListAdapter listAdapter;
+    private OCursorListAdapter mAdapter = null;
+    private boolean syncRequested = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        setHasSyncStatusObserver(KEY, this, db());
         return inflater.inflate(R.layout.common_listview, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setHasSwipeRefreshView(view, R.id.swipe_container, this);
         mView = view;
         listView = (ListView) mView.findViewById(R.id.listview);
-        listAdapter = new OCursorListAdapter(getActivity(), null, android.R.layout.simple_list_item_1);
-        listView.setAdapter(listAdapter);
-        listAdapter.setOnViewBindListener(this);
-        setHasSyncStatusObserver(TAG, this, db());
+        mAdapter = new OCursorListAdapter(getActivity(), null, android.R.layout.simple_list_item_1);
+        mAdapter.setOnViewBindListener(this);
+        mAdapter.setHasSectionIndexers(true, "name");
+        listView.setAdapter(mAdapter);
+        listView.setFastScrollAlwaysVisible(true);
+        listView.setOnItemClickListener(this);
+        //setHasSyncStatusObserver(TAG, this, db());
+        setHasFloatingButton(view, R.id.fabButton, listView, this);
         getLoaderManager().initLoader(0, null, this);
 
     }
@@ -68,27 +88,40 @@ public class Tasks extends BaseFragment implements LoaderManager.LoaderCallbacks
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        listAdapter.changeCursor(data);
+        mAdapter.changeCursor(data);
         if (data.getCount() > 0) {
-            OControls.setGone(mView, R.id.loadingProgress);
-            OControls.setVisible(mView, R.id.swipe_container);
-            OControls.setGone(mView, R.id.data_list_no_item);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    OControls.setGone(mView, R.id.loadingProgress);
+                    OControls.setVisible(mView, R.id.swipe_container);
+                    OControls.setGone(mView, R.id.data_list_no_item);
+                    setHasSwipeRefreshView(mView, R.id.swipe_container, Tasks.this);
+                }
+            }, 500);
         } else {
-            OControls.setGone(mView, R.id.loadingProgress);
-            OControls.setGone(mView, R.id.swipe_container);
-            OControls.setVisible(mView, R.id.data_list_no_item);
-            OControls.setText(mView, R.id.title, "No Tasks found");
-            OControls.setText(mView, R.id.subTitle, "Swipe to check new tasks");
-        }
-        if (db().isEmptyTable()) {
-            // Request for sync
-            onRefresh();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    OControls.setGone(mView, R.id.loadingProgress);
+                    OControls.setGone(mView, R.id.swipe_container);
+                    OControls.setVisible(mView, R.id.data_list_no_item);
+                    setHasSwipeRefreshView(mView, R.id.data_list_no_item, Tasks.this);
+                    OControls.setImage(mView, R.id.icon, R.drawable.ic_action_notes_content);
+                    OControls.setText(mView, R.id.title, _s(R.string.label_no_tasks_found));
+                    OControls.setText(mView, R.id.subTitle, "");
+                }
+            }, 500);
+            if (db().isEmptyTable() && !syncRequested) {
+                syncRequested = true;
+                onRefresh();
+            }
         }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        listAdapter.changeCursor(null);
+        mAdapter.changeCursor(null);
     }
 
     @Override
@@ -99,25 +132,82 @@ public class Tasks extends BaseFragment implements LoaderManager.LoaderCallbacks
     @Override
     public List<ODrawerItem> drawerMenus(Context context) {
         List<ODrawerItem> menu = new ArrayList<>();
-        menu.add(new ODrawerItem(KEY).setTitle("Tasks").setInstance(new Tasks()));
+        menu.add(new ODrawerItem(KEY).setTitle(OResource.string(context, R.string.sync_label_tasks))
+                .setIcon(R.drawable.ic_action_notes_content)
+                .setInstance(new Tasks()));
         return menu;
     }
 
     @Override
-    public void onStatusChange(Boolean changed) {
-        if(changed){
-            getLoaderManager().restartLoader(0, null, this);
-        }
+    public void onStatusChange(Boolean refreshing) {
+        // Sync Status
+        getLoaderManager().restartLoader(0, null, this);
     }
 
     @Override
     public void onRefresh() {
         if (inNetwork()) {
             parent().sync().requestSync(ProjectTask.AUTHORITY);
+            setSwipeRefreshing(true);
+        } else {
+            hideRefreshingProgress();
+            Toast.makeText(getActivity(), _s(R.string.toast_network_required), Toast.LENGTH_LONG)
+                    .show();
         }
     }
 
+    // Hasta aquí los metodos del ejemplo
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.menu_partners, menu);
+        setHasSearchView(this, menu, R.id.menu_partner_search);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    public boolean onSearchViewTextChange(String newFilter) {
+        mCurFilter = newFilter;
+        getLoaderManager().restartLoader(0, null, this);
+        return true;
+    }
+
+    @Override
+    public void onSearchViewClose() {
+        // nothing to do
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fabButton:
+                loadActivity(null);
+                break;
+        }
+    }
+
+    private void loadActivity(ODataRow row) {
+        Bundle data = new Bundle();
+        if (row != null) {
+            data = row.getPrimaryBundleData();
+        }
+        //data.putString(CustomerDetails.KEY_PARTNER_TYPE, mType.toString());
+        IntentUtils.startActivity(getActivity(), TasksDetails.class, data);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ODataRow row = OCursorUtils.toDatarow((Cursor) mAdapter.getItem(position));
+        loadActivity(row);
+    }
 }
+
+
--- app/src/main/java/com/odoo/addons/projects/models/ProjectTask.java
+++ app/src/main/java/com/odoo/addons/projects/models/ProjectTask.java
@@ -3,10 +3,14 @@ package com.odoo.addons.projects.models;
 import android.content.Context;
 import android.net.Uri;
 
+import com.odoo.base.addons.res.ResUsers;
 import com.odoo.core.orm.OModel;
 import com.odoo.core.orm.fields.OColumn;
+import com.odoo.core.orm.fields.types.ODate;
+import com.odoo.core.orm.fields.types.ODateTime;
 import com.odoo.core.orm.fields.types.OText;
 import com.odoo.core.orm.fields.types.OVarchar;
+import com.odoo.core.rpc.helper.ODomain;
 import com.odoo.core.support.OUser;
 
 /**
@@ -19,7 +23,10 @@ public class ProjectTask extends OModel {
 
     OColumn name = new OColumn("Name", OVarchar.class).setSize(100);
     OColumn project_id = new OColumn("Project", ProjectProject.class, OColumn.RelationType.ManyToOne);
+    //OColumn user_id = new OColumn("Assigned", ResUsers.class, OColumn.RelationType.ManyToOne);
+
     OColumn description = new OColumn("Description", OText.class);
+    OColumn date_deadline = new OColumn("Date", ODate.class);
 
     public ProjectTask(Context context, OUser user) {
         super(context, "project.task", user);
@@ -28,5 +35,12 @@ public class ProjectTask extends OModel {
     @Override
     public Uri uri() {
         return buildURI(AUTHORITY);
+    }
+
+    @Override
+    public ODomain defaultDomain() {
+        ODomain domain = new ODomain();
+        domain.add("user_id", "=", getUser().getUserId());
+        return domain;
     }
 }
