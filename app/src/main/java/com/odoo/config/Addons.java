6bd464731da1ee9edbea77ac867e02a18510fbbc
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-11-22 10:07:10 -0500

    Enel prueba comit 2

--- app/src/main/java/com/odoo/config/Addons.java
+++ app/src/main/java/com/odoo/config/Addons.java
@@ -46,7 +46,6 @@ public class Addons extends AddonsHelper {
     OAddon payment = new OAddon(AccountPayment.class);
 */
     OAddon encuesta = new OAddon(EncuestaF.class).setDefault();
-    //Prueba
     //OAddon surveysurvey = new OAddon(SurveySurvey.class);
     //OAddon surveypage = new OAddon(SurveyPage.class);
     //OAddon surveyquestion = new OAddon(SurveyQuestion.class);
